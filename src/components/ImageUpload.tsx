73c5a72c3134cd5ae841c746ebf1fedb4917e619
Aio <AioDev.JS@gmail.com>
2021-09-04 19:19:44 +0900

    Dropzone Image Upload

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import { useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { useRecoilState } from 'recoil';
+import { textState } from '../recoil/atom';
 
 const ImageUpload = () => {
   const [Image, setImage] = useState<Object | null>(null);
@@ -7,6 +10,14 @@ const ImageUpload = () => {
     e.preventDefault();
     console.log(e);
   };
+  const [text, setText] = useRecoilState(textState);
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+  const files = acceptedFiles.map((file) => (
+    <li key={file.name}>
+      {file.name} - {file.size} bytes
+    </li>
+  ));
 
   const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files?.[0]) {
@@ -27,19 +38,25 @@ const ImageUpload = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="wordImg">분석할 사진</label>
+    <section>
+      <div
+        className=" h-40 border-2 px-4 py-6 border-mint  hover:bg-mint hover:text-white cursor-pointer "
+        {...getRootProps({})}
+      >
         <input
-          id="wordImg"
-          type="file"
-          accept="image/*"
-          alt="분석할 단어 사진"
-          onChange={handleUpload}
+          {...getInputProps({
+            id: 'wordImg',
+            type: 'file',
+            accept: 'image/*',
+            alt: '분석할 단어 사진',
+            onChange: handleUpload,
+          })}
         />
-        <button type="submit">제출</button>
-      </form>
-    </div>
+        <label htmlFor="wordImg">분석할 사진</label>
+        <p>드래그드랍 또는 클릭으로 단어 사진 업로드</p>
+      </div>
+      <ul>{files}</ul>
+    </section>
   );
 };
 
