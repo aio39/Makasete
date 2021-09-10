bb1163d946f73355a03242938b4864ab8386d3d7
Aio <AioDev.JS@gmail.com>
2021-09-11 01:11:33 +0900

    gcp ocr 주석, divide 삭제

--- serverless/index.js
+++ serverless/index.js
@@ -1,6 +1,7 @@
+const { memoryUsage } = require('process');
+
 exports.test = async (request, response) => {
   const result = await Promise.resolve('ok');
-  
 
   response.status(200).send(result);
 };
@@ -16,37 +17,6 @@ const axios = require('axios');
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
-const sharp = require('sharp');
-
-const papago = async (text) => {
-  try {
-    const result = await axios.post(
-      'https://openapi.naver.com/v1/papago/n2mt',
-      {
-        source: 'ja',
-        target: 'ko',
-        text: text,
-      },
-      {
-        headers: {
-          'X-Naver-Client-Id': process.env.D2_ID,
-          'X-Naver-Client-Secret': process.env.D2_SECRET,
-        },
-      }
-    );
-    console.log(result.data);
-    return result.data.message.result.translatedText;
-  } catch (error) {
-    if (error.response) {
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      console.log(error.request);
-    }
-    return null;
-  }
-};
 
 const { Translate } = require('@google-cloud/translate').v2;
 const translate = async (textList) => {
@@ -59,7 +29,6 @@ const translate = async (textList) => {
   };
 
   const [translation] = await translate.translate(textList, option);
-  console.log(translation);
   return translation;
 };
 
@@ -70,42 +39,33 @@ exports.ocr = (req, res) => {
     process.env.HOST || 'http://localhost:3000'
   );
   res.set('Access-Control-Allow-Credentials', 'true');
-  res.set('Access-Control-Allow-Methods', 'POST');
+  res.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.set('Access-Control-Allow-Headers', 'Authorization');
   res.set('Access-Control-Max-Age', '3600');
   if (req.method !== 'POST') {
     return res.status(405).end();
   }
   const busboy = new Busboy({ headers: req.headers });
-  const tmpdir = os.tmpdir();
 
   const fields = {};
   const uploads = {};
-
-  let divide = 1; // default divide is 1
-
+  // not File 필드 string 값
   busboy.on('field', (fieldname, val, a, b, c, d) => {
-    if (fieldname === 'divide') divide = parseInt(val);
     fields[fieldname] = val;
   });
 
-  const fileWrites = [];
-  const chunks = [];
-  // This code will process each file uploaded.
+  const tmpdir = os.tmpdir();
+  const fileWritePromises = [];
+  let imageData;
   busboy.on('file', (fieldname, file, filename) => {
-    // Note: os.tmpdir() points to an in-memory file system on GCF
-    // Thus, any files in it must fit in the instance's memory.
-
-    console.log(`Processed file ${filename}`);
-    const filepath = path.join(tmpdir, filename);
+    const filepath = path.join(tmpdir, filename); // 메모리상 dir
     uploads[fieldname] = filepath;
-
     const writeStream = fs.createWriteStream(filepath);
     file.pipe(writeStream);
 
     const promise = new Promise((resolve, reject) => {
       file.on('data', (data) => {
-        chunks.push(data);
+        imageData = data;
       });
       file.on('end', () => {
         writeStream.end();
@@ -113,110 +73,78 @@ exports.ocr = (req, res) => {
       writeStream.on('finish', resolve);
       writeStream.on('error', reject);
     });
-    fileWrites.push(promise);
+    fileWritePromises.push(promise);
   });
 
   // Triggered once all uploaded files are processed by Busboy.
   // We still need to wait for the disk writes (saves) to complete.
+  const result = [];
   busboy.on('finish', async () => {
-    await Promise.all(fileWrites);
+    for (const file in uploads) {
+      fs.unlinkSync(uploads[file]);
+    }
 
+    await Promise.all(fileWritePromises);
     /**
      * TODO(developer): Process saved files here
      */
-    console.log(chunks[0]);
-
     const client = new vision.ImageAnnotatorClient();
-    const metadata = await sharp(chunks[0]).rotate().metadata();
-    const crop = [];
-
-    await Promise.all(
-      Array(divide)
-        .fill()
-        .map(async (_, idx) => {
-          crop[idx] = await sharp(chunks[0])
-            .extract({
-              left: 0,
-              top: (metadata.height / divide) * (divide - (idx + 1)),
-              height: metadata.height / divide,
-              width: metadata.width,
-            })
-            .toBuffer();
-        })
-    );
-
-    const detectedTextList = await Promise.all(
-      crop.map((chunk) =>
-        client
-          .textDetection({
-            image: {
-              content: new Uint8Array(chunk),
-            },
-          })
-          .catch((err) => {
-            console.err(err);
-          })
-      )
-    );
-
-    for (const file in uploads) {
-      console.log(uploads[file]);
-      console.log(fileWrites);
-      fs.unlinkSync(uploads[file]);
-    }
-    console.log(detectedTextList);
-
-    const finalResult = [];
-
-    await Promise.all(
-      detectedTextList.map(async (chunk, resultIdx) => {
-        const rawText = chunk[0].fullTextAnnotation.text;
-        const kanjiText = rawText
-          .replace(/[\w)(|]/gi, '') // 특수 문자 제거
-          .replace(/\n/g, ' ') // 줄바꿈 제거
-          .replace(/ +(?= )/g, '') // 2칸 이상 공백 제거
-          .trim(); // 좌우 공백 제거
-
-        const delimiter = '*';
-        const kanjiTextList = kanjiText.split(' ');
-
-        const [
-          {
-            data: { converted: hiraganaText },
-          },
-          hangulTextList,
-        ] = await Promise.all([
-          axios
-            .post('https://labs.goo.ne.jp/api/hiragana', {
-              app_id:
-                '5f82101255bc786575ef667938d33a5f8afccc2e54aa4638dd81230aa06c26e8',
-              sentence: kanjiText.replace(/( * )/g, delimiter),
-              output_type: 'hiragana',
-            })
-            .catch((err) => {
-              console.log(err);
-            }),
-          translate(kanjiText.replace(/ /g, '。 ').split(' ')),
-        ]);
-
-        const hiraganaTextList = hiraganaText
-          .replace(/( * )+/g, '')
-          .split(delimiter);
-
-        finalResult[resultIdx] = [];
-        Array(kanjiTextList.length)
-          .fill()
-          .forEach((_, idx) => {
-            finalResult[resultIdx].push([
-              kanjiTextList[idx],
-              hiraganaTextList[idx],
-              ...(hangulTextList ? [hangulTextList[idx].slice(0, -1)] : []),
-            ]);
-          });
+    const detectedText = await client
+      .textDetection({
+        image: {
+          content: new Uint8Array(imageData),
+        },
       })
-    );
+      .catch((err) => {
+        console.err(err);
+      })
+      .finally(() => {
+        imageData = null;
+      });
+    const rawText = detectedText[0].fullTextAnnotation.text;
+    const kanjiText = rawText
+      .replace(/[\w)(|]/gi, '') // 특수 문자 제거
+      .replace(/\n/g, ' ') // 줄바꿈 제거
+      .replace(/ +(?= )/g, '') // 2칸 이상 공백 제거
+      .trim(); // 좌우 공백 제거
 
-    res.send(finalResult);
+    const delimiter = '*';
+    const kanjiTextList = kanjiText.split(' ');
+
+    const [
+      {
+        data: { converted: hiraganaText },
+      },
+      hangulTextList,
+    ] = await Promise.all([
+      axios
+        .post('https://labs.goo.ne.jp/api/hiragana', {
+          app_id:
+            '5f82101255bc786575ef667938d33a5f8afccc2e54aa4638dd81230aa06c26e8',
+          sentence: kanjiText.replace(/( * )/g, delimiter),
+          output_type: 'hiragana',
+        })
+        .catch((err) => {
+          console.log(err);
+        }),
+      translate(kanjiText.replace(/ /g, '。 ').split(' ')),
+    ]);
+
+    const hiraganaTextList = hiraganaText
+      .replace(/( * )+/g, '')
+      .split(delimiter);
+
+    Array(kanjiTextList.length)
+      .fill()
+      .forEach((_, idx) => {
+        result.push([
+          kanjiTextList[idx],
+          hiraganaTextList[idx],
+          ...(hangulTextList ? [hangulTextList[idx].slice(0, -1)] : []),
+        ]);
+      });
+    console.log(memoryUsage());
+    res.send(result);
   });
 
   busboy.end(req.rawBody);
