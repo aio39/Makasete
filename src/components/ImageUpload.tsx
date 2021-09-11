7ffc15e613b756f2cbd6fd09125c1b5c495a0a98
Aio <AioDev.JS@gmail.com>
2021-09-12 00:19:48 +0900

    offScreenCanvas 미지원 브라우저 분기처리

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -10,12 +10,14 @@ import { Crop } from 'react-image-crop';
 import { useSetRecoilState } from 'recoil';
 import imageToJpegDataUrlWorker from '../pwa/ImageToJpegDataUrl';
 import { isLoadingOcrState, textState } from '../recoil/atom';
+import { dataURItoBlob } from '../util/dataURItoBlob';
 import rotateDataUrlOfImage from '../util/rotateImage';
 import CropZone from './uploadPart/CropZone';
 import ImageInputZone from './uploadPart/ImageInputZone';
 
 const ImageUpload = () => {
   const worker = useMemo(() => {
+    if (!window.OffscreenCanvas) return null;
     const worker = new Worker(imageToJpegDataUrlWorker);
     worker.onmessage = (m) => {
       setCroppedImageDataUrlList((pre) => [...pre, m.data]);
@@ -36,13 +38,13 @@ const ImageUpload = () => {
 
   useEffect(() => {
     return () => {
-      worker.terminate();
+      worker?.terminate();
     };
   }, [worker]);
 
   const setWorkerImage = useCallback(
     (imageUrl) => {
-      worker.postMessage({ imageUrl });
+      worker?.postMessage({ imageUrl });
     },
     [worker]
   );
@@ -51,12 +53,17 @@ const ImageUpload = () => {
     setIsLoadingOcr(true);
 
     const result = await Promise.all(
-      croppedImageDataUrlList.map(async (dataUrl) => {
+      croppedImageDataUrlList.map(async (dataOrObjectUrl) => {
         const params = new FormData();
-        console.time('fetch');
-        const blob = await fetch(dataUrl).then((r) => r.blob());
-        console.timeEnd('fetch');
-        params.append('image', blob);
+        if (window.OffscreenCanvas) {
+          console.time('fetch');
+          const blob = await fetch(dataOrObjectUrl).then((r) => r.blob());
+          console.timeEnd('fetch');
+          params.append('image', blob);
+        } else {
+          params.append('image', dataURItoBlob(dataOrObjectUrl));
+        }
+
         try {
           const result = await axios.post<string[][]>(
             process.env.REACT_APP_OCR_URL as string,
@@ -110,36 +117,40 @@ const ImageUpload = () => {
     //  레티나 디스플레이에서 추가 픽셀 밀접도로 해상도를 올릴 수 있다.
     const pixelRatio = window.devicePixelRatio;
 
-    // canvas.width = crop.width * pixelRatio * scaleX;
-    // canvas.height = crop.height * pixelRatio * scaleY;
+    if (window.OffscreenCanvas) {
+      const drawData = {
+        crop,
+        scaleX,
+        scaleY,
+        pixelRatio,
+      };
+      worker?.postMessage({ drawData });
+    } else {
+      const canvas = document.createElement('canvas') as any;
+      const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+      canvas.width = crop.width * pixelRatio * scaleX;
+      canvas.height = crop.height * pixelRatio * scaleY;
 
-    // ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
-    // ctx.imageSmoothingQuality = 'high';
+      ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
+      ctx.imageSmoothingQuality = 'high';
 
-    // ctx.drawImage(
-    //   image,
-    //   crop.x * scaleX,
-    //   crop.y * scaleY,
-    //   crop.width * scaleX,
-    //   crop.height * scaleY,
-    //   0,
-    //   0,
-    //   crop.width * scaleX,
-    //   crop.height * scaleY
-    // );
+      ctx.drawImage(
+        image,
+        crop.x * scaleX,
+        crop.y * scaleY,
+        crop.width * scaleX,
+        crop.height * scaleY,
+        0,
+        0,
+        crop.width * scaleX,
+        crop.height * scaleY
+      );
 
-    const drawData = {
-      crop,
-      scaleX,
-      scaleY,
-      pixelRatio,
-    };
-
-    worker.postMessage({ drawData });
-    // setCroppedImageDataUrlList((pre) => [
-    //   ...pre,
-    //   canvas.toDataURL('image/jpeg', 0.7) as string,
-    // ]);
+      setCroppedImageDataUrlList((pre) => [
+        ...pre,
+        canvas.toDataURL('image/jpeg', 0.7) as string,
+      ]);
+    }
   }, [completedCrop, worker]);
 
   const handleDeleteCrop = (e: any) => {
@@ -156,7 +167,7 @@ const ImageUpload = () => {
       cropTargetImageRef.current?.getAttribute('src') as string
     );
     cropTargetImageRef.current?.setAttribute('src', newDataUrl);
-    worker.postMessage({ imageUrl: newDataUrl });
+    worker?.postMessage({ imageUrl: newDataUrl });
   };
 
   return (
