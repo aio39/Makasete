589f0d93a189e9313bae7805682fba464847892d
Aio <AioDev.JS@gmail.com>
2021-09-13 19:39:48 +0900

    Footer 갱신

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,32 @@
+import { FC, ReactElement } from 'react';
+import { GoMarkGithub } from 'react-icons/go';
+
+const iconList = [{ icon: <GoMarkGithub />, link: 'https://github.com/aio39' }];
+
+const IconLink: FC<{ iconData: { icon: ReactElement; link: string } }> = ({
+  iconData: { icon, link },
+}) => {
+  return (
+    <ul>
+      <a href={link} className="text-2xl">
+        {icon}
+      </a>
+    </ul>
+  );
+};
+
 const Footer = () => {
-  return <footer className="bg-gray-400 w-full h-28 ">0.4</footer>;
+  return (
+    <footer className=" w-full h-14 flex flex-col justify-center items-center  px-8 mb-4">
+      <div className="w-full  border-t-2 border-gray-300  "></div>
+      <small className="text-base my-2">
+        Copyright @ {new Date().getFullYear()}, All Right Reserved Aio
+      </small>
+      {iconList.map((iconData) => {
+        return <IconLink iconData={iconData} />;
+      })}
+    </footer>
+  );
 };
 
 export default Footer;
