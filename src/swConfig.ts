164dd3ef4a4619f4d1d4965dae7f85b2833c4804
Aio <AioDev.JS@gmail.com>
2021-09-29 16:34:42 +0900

    pws 자동 업데이트

--- src/swConfig.ts
+++ src/swConfig.ts
@@ -1,8 +1,20 @@
 const swConfig = {
   onUpdate: (registration: ServiceWorkerRegistration): void => {
-    registration.unregister().then(() => {
-      window.location.reload();
-    });
+    const waitingServiceWorker = registration.waiting;
+
+    if (waitingServiceWorker) {
+      waitingServiceWorker.addEventListener('statechange', (event) => {
+        if ((event.target as any).state === 'activated') {
+          window.location.reload();
+        }
+      });
+      waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
+    }
+    // registration.unregister().then(() => {
+    //   window.alert(
+    //     '새로운 버전으로 업데이트 가능합니다. 열려져있는 MAKASETE 페이지를 전부 닫고 재실행해주세요.'
+    //   );
+    // });
   },
 };
 
