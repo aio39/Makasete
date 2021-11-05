6ce2fea4c4ef918401685c38dd7bf64966057fcb
Aio <AioDev.JS@gmail.com>
2021-11-05 14:39:11 +0900

    옵션 css 개선

--- src/components/header/OptionModal.tsx
+++ src/components/header/OptionModal.tsx
@@ -20,50 +20,64 @@ export const OptionModal: FC<{}> = () => {
 
   return (
     <div>
-      <h2 className="text-4xl my-6  font-mono font-bold">マカセテ</h2>
+      <h2 className="text-4xl my-6  font-mono font-bold text-center">
+        マカセテ
+      </h2>
       <div className="my-4">
-        <label>사전 오픈 방식</label>
+        <label className="mr-4">사전 오픈 방식</label>
         <input
           type="radio"
           checked={openDictMode === 'modal'}
           value="modal"
           id="radioModal"
           onChange={() => setOpenDictMode('modal')}
+          className="mr-4"
         />
-        <label htmlFor="radioModal">모달창 </label>
+        <label htmlFor="radioModal" className="mr-4">
+          모달창
+        </label>
         <input
           type="radio"
           checked={openDictMode === 'tab'}
           value="tab"
           id="radioTab"
           onChange={() => setOpenDictMode('tab')}
+          className="mr-4"
         />
         <label htmlFor="radioTab">새 탭 </label>
       </div>
       <div className="my-4">
-        <label>FPS</label>
+        <label className="mr-4">FPS</label>
         <input
           type="radio"
           checked={cropFps === 30}
           value="30"
           id="radioFps30"
           onChange={() => setCropFpsHandler(30)}
+          className="mr-4"
         />
-        <label htmlFor="radioFps30">30 </label>
+
+        <label htmlFor="radioFps30" className="mr-4">
+          30
+        </label>
         <input
           type="radio"
           checked={cropFps === 60}
           value="60"
           id="radioFps60"
           onChange={() => setCropFpsHandler(60)}
+          className="mr-4"
         />
-        <label htmlFor="radioFps60">60</label>
+        <label htmlFor="radioFps60" className="mr-4">
+          60
+        </label>
         <input
           type="radio"
           checked={cropFps === 120}
           value="120"
           id="radioFps120"
           onChange={() => setCropFpsHandler(120)}
+          className="mr-4"
         />
         <label htmlFor="radioFps120">120</label>
       </div>
